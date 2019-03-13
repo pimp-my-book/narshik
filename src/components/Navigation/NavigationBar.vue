c343c453ec1c0a9aa1b3bc7c35870e7fdd4df59d
amodinho <u14284783@tuks.co.za>
2019-03-13 17:45:48 +0200

    added color to the navbar

--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -1,6 +1,12 @@
 <template>
-    <b-navbar toggleable='lg' type='dark' variant='info'>
-        <b-navbar-brand href='#'>GOA</b-navbar-brand>
+    <b-navbar toggleable='lg' type='dark' >
+        <b-navbar-brand href='#'>GOA's Design System</b-navbar-brand>
+        <b-navbar-toggle target='nav_collapse' />
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item right>Get Started</b-nav-item>
+            <b-nav-item right>v0.0.1</b-nav-item>
+        </b-navbar-nav>
     </b-navbar>
 </template>
 
@@ -9,4 +15,14 @@ export default {
     name: 'NavigationBar'
 }
 </script>
+
+ <style  scoped>
+
+ nav {
+     background-color: #08160A
+ }
+
+</style>
+
+
 
