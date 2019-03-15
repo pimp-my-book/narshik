eb19e8973d84f9e9fdd6711e996777ebc1958b36
amodinho <u14284783@tuks.co.za>
2019-03-15 10:18:13 +0200

    added style path and commented the router.

--- src/router.js
+++ src/router.js
@@ -1,3 +1,4 @@
+/*
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
@@ -17,7 +18,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */  //'./views/About.vue')
+    /*
     },
     {
       path: '/start',
@@ -26,3 +28,9 @@ export default new Router({
     }
   ]
 })
+
+*/
+
+
+
+
