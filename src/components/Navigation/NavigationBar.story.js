10f7af4bd08838e607e75710f2943f373fd24ce8
AmoDinho <u14284783@tuks.co.za>
2019-09-10 14:17:16 +0200

    Trying to fix this webpack issue

--- src/components/Navigation/NavigationBar.story.js
+++ src/components/Navigation/NavigationBar.story.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
 import NavigationBar from './NavigationBar.vue'
+import { template } from 'handlebars'
+
+
 
 storiesOf('Navbar', module)
-    .add('A nav bar', () => '<NavigationBar/>')
+    .add('A nav bar', () => {
+        return {
+
+            components: { NavigationBar },
+            template: `<NavigationBar/>`
+        }
+    })
