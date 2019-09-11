22c2b524124e6dda2efec5f925c271bbf23014b8
AmoDinho <u14284783@tuks.co.za>
2019-09-11 12:32:07 +0200

    removed all files and restarting project

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/vue'
-
-function loadStories() {
-    const req = require.context('../src', true, /\.story\.js$/)
-    req.keys().forEach(req)
-
-}
-
-configure(loadStories, module)
