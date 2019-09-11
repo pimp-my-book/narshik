1edd31ca51f4b0457d61f342f14edd4f8e54a9e6
AmoDinho <u14284783@tuks.co.za>
2019-09-11 12:45:39 +0200

    added a heading component

---
+++ src/components/HeadingOne.vue
@@ -0,0 +1,19 @@
+<template>
+    <p :class="[small ? 'text-sm font-body' : 'text-lg font-body' ]">
+    <a
+    :href="url" 
+    class="no-underline ">    
+    {{text}}
+    </a>
+    </p>
+</template>
+<script>
+export default {
+    name:'HeadingOne',
+    props: {
+        text: String,
+        small: Boolean,
+        url: String 
+    }
+}
+</script>
