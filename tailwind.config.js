87901335bb8864159484773011808da04ed8c3b8
AmoDinho <u14284783@tuks.co.za>
2019-09-13 11:47:03 +0200

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,88 @@
 module.exports = {
   theme: {
-    extend: {}
+    colors: {
+      'transparent': 'transparent',
+
+      'black': '#22292f',
+      'grey-darker': '#434343',
+      'grey-dark': '#52524E',
+      'grey': '#9A9B94',
+      'grey-light': '#D4D6C8',
+      'grey-lighter': '#E9E9E5',
+      'white': '#ffffff',
+      'white-lighter': '#FCF5EE',
+
+      'red-dark': '#E71D36',
+      'red': '#FF4545',
+      'red-light': '#FF9867',
+      'red-lighter': '#FFBF87',
+      'red-lightest': '#FFEDB2',
+
+      'orange-darker': '#FF941F',
+      'orange-dark': '#FFA401',
+      'orange': '#FFB300',
+      'orange-light': '#FFBF00',
+      'orange-lighter': '#FFC516',
+
+      'yellow-darkest': '#453411',
+      'yellow-darker': '#684f1d',
+      'yellow-dark': '#f2d024',
+      'yellow': '#ffed4a',
+      'yellow-light': '#fff382',
+      'yellow-lighter': '#fff9c2',
+      'yellow-lightest': '#fcfbeb',
+
+
+      'green-darker': '#08160A',
+      'green-dark': '#247A2E',
+      'green': '#4ECA5D',
+      'green-light': '#A0FFAB',
+      'green-lighter': '#D7FFF1',
+      'green-gradient': 'linear-gradient(179.9deg, #A0FFAB 23.23%, #94B5E3 99.68%)',
+
+
+      'teal-darkest': '#0d3331',
+      'teal-darker': '#20504f',
+      'teal-dark': '#38a89d',
+      'teal': '#4dc0b5',
+      'teal-light': '#64d5ca',
+      'teal-lighter': '#a0f0ed',
+      'teal-lightest': '#e8fffe',
+
+      'blue-darkest': '#12283a',
+      'blue-darker': '#1c3d5a',
+      'blue-dark': '#2779bd',
+      'blue': '#3490dc',
+      'blue-light': '#6cb2eb',
+      'blue-lighter': '#bcdefa',
+      'blue-lightest': '#eff8ff',
+
+      'indigo-darkest': '#191e38',
+      'indigo-darker': '#2f365f',
+      'indigo-dark': '#5661b3',
+      'indigo': '#6574cd',
+      'indigo-light': '#7886d7',
+      'indigo-lighter': '#b2b7ff',
+      'indigo-lightest': '#e6e8ff',
+
+      'purple-darkest': '#21183c',
+      'purple-darker': '#382b5f',
+      'purple-dark': '#794acf',
+      'purple': '#9561e2',
+      'purple-light': '#a779e9',
+      'purple-lighter': '#d6bbfc',
+      'purple-lightest': '#f3ebff',
+
+      'pink-darkest': '#451225',
+      'pink-darker': '#6f213f',
+      'pink-dark': '#eb5286',
+      'pink': '#f66d9b',
+      'pink-light': '#fa7ea8',
+      'pink-lighter': '#ffbbca',
+      'pink-lightest': '#ffebef',
+
+
+    }
   },
   variants: {},
   plugins: []
