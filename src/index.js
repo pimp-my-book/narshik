83dd3becc83be5e193631705082b666d478cdb79
AmoDinho <u14284783@tuks.co.za>
2019-09-17 12:38:39 +0200

    exported the components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 require('./assets/tailwind.css')
-import HeadingOne from './components/HeadingOne.vue'
+import DisplayXLarge from './components/typogrpahy/DisplayXLarge.vue'
+import DisplayLarge from './components/typogrpahy/DisplayLarge.vue'
 
 
 const Components = {
-    HeadingOne
+    DisplayXLarge,
+    DisplayLarge
 }
 Object.keys(Components).forEach(name => {
     Vue.component(name, Components[name])
