e45b726fb0f66c8a8f09b1261cf4ea4781769f45
AmoDinho <u14284783@tuks.co.za>
2019-09-17 13:03:00 +0200

    exported FeatureText

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ require('./assets/tailwind.css')
 import DisplayXLarge from './components/typogrpahy/DisplayXLarge.vue'
 import DisplayLarge from './components/typogrpahy/DisplayLarge.vue'
 import DisplaySmall from './components/typogrpahy/DisplaySmall.vue'
+import FeatureText from './components/typogrpahy/FeatureText.vue'
 
 
 const Components = {
     DisplayXLarge,
     DisplayLarge,
-    DisplaySmall
+    DisplaySmall,
+    FeatureText
 }
 Object.keys(Components).forEach(name => {
     Vue.component(name, Components[name])
