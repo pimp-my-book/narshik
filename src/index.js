09ff25e93683f5d3fa93fc4c55a8f2deed543ccb
AmoDinho <u14284783@tuks.co.za>
2019-09-17 12:55:17 +0200

    exported DisplaySmall

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 require('./assets/tailwind.css')
 import DisplayXLarge from './components/typogrpahy/DisplayXLarge.vue'
 import DisplayLarge from './components/typogrpahy/DisplayLarge.vue'
+import DisplaySmall from './components/typogrpahy/DisplaySmall.vue'
 
 
 const Components = {
     DisplayXLarge,
-    DisplayLarge
+    DisplayLarge,
+    DisplaySmall
 }
 Object.keys(Components).forEach(name => {
     Vue.component(name, Components[name])
