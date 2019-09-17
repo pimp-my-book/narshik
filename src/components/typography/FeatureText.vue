126b7cca0846194f526efb783e0781cd0ad5ae04
AmoDinho <u14284783@tuks.co.za>
2019-09-17 12:59:51 +0200

    added feature text component

---
+++ src/components/typography/FeatureText.vue
@@ -0,0 +1,13 @@
+<template>
+  <p :class="[small ? 'text-sm font-body' : 'text-lg font-body' ]">{{text}}</p>
+</template>
+<script>
+export default {
+  name: "FeatureText",
+  props: {
+    text: String,
+    small: Boolean,
+    url: String
+  }
+};
+</script>
