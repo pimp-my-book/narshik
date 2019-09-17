345d090c4de3238bcd25f342c4c0568277afa31d
AmoDinho <u14284783@tuks.co.za>
2019-09-17 12:35:04 +0200

    removed the require statement

--- src/components/typography/DisplayXLarge.story.js
+++ src/components/typography/DisplayXLarge.story.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
 import DisplayXLarge from './DisplayXLarge.vue'
-require('../../assets/tailwind.css')
+
 storiesOf('Display X Large', module)
     .add('Xtra large Heading', () => {
         return {
