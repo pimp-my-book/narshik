de9ba55d8ba17a2c3927f4d0a3ae64be7f026156
AmoDinho <u14284783@tuks.co.za>
2019-09-18 11:10:51 +0200

    added test for input

---
+++ tests/unit/Input.spec.js
@@ -0,0 +1,10 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import Input from '../../src/components/inputs/Input.vue'
+
+afterEach(cleanup)
+
+describe('<Input/>', () => {
+    test('Accepts text prop', () => {
+
+    })
+})
