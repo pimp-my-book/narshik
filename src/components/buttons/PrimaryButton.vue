8500f57698e1ced0430fcbf8ed2fc408f2dba5f7
AmoDinho <u14284783@tuks.co.za>
2019-09-18 12:33:19 +0200

    added primary button component

---
+++ src/components/buttons/PrimaryButton.vue
@@ -0,0 +1,35 @@
+<!--- 
+This is the primary button
+props: 
+url - need if you need forward to a link
+text - accepts button text
+regular - changes the button size
+actions:
+forward - redirects using the url prop
+-->
+
+<template>
+  <button
+    type="submit"
+    url
+    @click="forward"
+    :class="[regular ? ' bg-green hover:text-green-dark hover:bg-green-lighter font-body py-2 px-4  text-white rounded focus:bg-green-lighter focus:outline-none' 
+    : 'bg-green   hover:text-green-dark hover:bg-green-lighter font-body py-4 px-6 sl:py-4 sl:px-6 s:py-2 s:px-2 text-white rounded focus:outline-none ']"
+  >{{text}}</button>
+</template>
+
+<script>
+export default {
+  name: "PrimaryButton",
+  props: {
+    text: String,
+    url: String,
+    regular: Boolean
+  },
+  methods: {
+    forward() {
+      this.$router.push(this.url);
+    }
+  }
+};
+</script>
