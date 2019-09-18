45cf55420c6066a56f8861850f25e4ec3531c666
AmoDinho <u14284783@tuks.co.za>
2019-09-18 12:31:38 +0200

    done with test case

--- tests/unit/PrimaryButton.spec.js
+++ tests/unit/PrimaryButton.spec.js
@@ -5,5 +5,11 @@ import PrimaryButton from '../../src/components/buttons/PrimaryButton'
 afterEach(cleanup)
 
 describe('<PrimaryButton/>', () => {
+    test('It accepts button text prop', () => {
 
+    })
+
+    test('It forwards the supplied url', () => {
+
+    })
 })
