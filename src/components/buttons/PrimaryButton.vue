564f5f2ca9fb257593e7dac413294608ba545968
AmoDinho <u14284783@tuks.co.za>
2019-09-18 13:44:21 +0200

    Added the loading icon

--- src/components/buttons/PrimaryButton.vue
+++ src/components/buttons/PrimaryButton.vue
@@ -16,7 +16,9 @@ forward - redirects using the url prop
     :class="[regular ? ' bg-green hover:text-green-dark hover:bg-green-lighter font-body py-2 px-4  text-white rounded focus:bg-green-lighter focus:outline-none' 
     : 'bg-green   hover:text-green-dark hover:bg-green-lighter font-body py-4 px-6 sl:py-4 sl:px-6 s:py-2 s:px-2 text-white rounded focus:outline-none ']"
   >
-    <div v-if="isLoading">I am loading</div>
+    <div v-if="isLoading">
+      <img src="../../assets/loader.svg" />
+    </div>
     <div v-else>{{text}}</div>
   </button>
 </template>
