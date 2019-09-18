3a55fb2fc45c3323182ab4e7d918fd1e2dc447e8
AmoDinho <u14284783@tuks.co.za>
2019-09-18 11:59:15 +0200

    added test case

---
+++ tests/unit/Textarea.spec.js
@@ -0,0 +1,10 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import Textarea from '../../src/components/inputs/Textarea.vue'
+
+afterEach(cleanup)
+
+describe('<Textarea/>', () => {
+    test('Accepts text input', () => {
+
+    })
+})
