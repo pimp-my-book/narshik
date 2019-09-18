22eea47cd17c2a097870074aba71c8bf18f35dd0
AmoDinho <u14284783@tuks.co.za>
2019-09-18 11:12:07 +0200

    created Input component

---
+++ src/components/inputs/Input.vue
@@ -0,0 +1,35 @@
+<template>
+    <input 
+    class="
+    bg-grey-lighter 
+    rounded-px 
+    py-2
+    px-4 
+    border-gray-500 
+    focus:outline-none 
+    focus:bg-white"
+    type="text"
+    :placeholder="placeholder"
+    @input="handleInput"
+    />
+</template>
+<script>
+export default {
+    name:'Input',
+    props: {
+    placeholder:String,
+   },
+   data(){
+       return {
+           value: ""
+       }
+      
+   },
+    methods:{
+           handleInput(e){
+               this.$emit('input', e.target.value)
+           }
+       }
+   
+}
+</script>
