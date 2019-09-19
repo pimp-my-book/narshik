8a747f6b9e7cb74c9ab8a97d28696205611aabf2
AmoDinho <u14284783@tuks.co.za>
2019-09-19 12:24:57 +0200

    added test case

---
+++ tests/unit/LinkButton.spec.js
@@ -0,0 +1,10 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import LinkButton from '../../src/components/buttons/LinkButton'
+
+afterEach(cleanup)
+
+describe('<LinkButton/>', () => {
+    test('It accepts text + url as prop', () => {
+
+    })
+})
