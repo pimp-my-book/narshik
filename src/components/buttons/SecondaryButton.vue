b86d2577904e87521a74a3461dd7f13334109ab9
AmoDinho <u14284783@tuks.co.za>
2019-09-19 11:51:06 +0200

    Changine the text colour

--- src/components/buttons/SecondaryButton.vue
+++ src/components/buttons/SecondaryButton.vue
@@ -4,8 +4,8 @@
     aria-label="secondary-button"
     @click="forward"
     :class="[regular 
-    ? ' hover:text-green-darker hover:bg-transparent bg-green-lighter font-body py-2 px-4 text-green-darker rounded border border-green hover:border-green hover:text-green focus:outline-none' 
-    : ' hover:text-green-darker  hover:bg-transparent bg-green-lighter font-body lg:py-4 lg:px-6 sm:py-4 sm:px-6 border hover:border-green hover:text-green  s:py-2 s:px-2 text-green-darker rounded focus:outline-none']"
+    ? ' hover:text-green-darker hover:bg-transparent bg-green-lighter font-body py-2 px-4 text-green-dark rounded border border-green hover:border-green hover:text-green focus:outline-none' 
+    : ' hover:text-green-darker  hover:bg-transparent bg-green-lighter font-body lg:py-4 lg:px-6 sm:py-4 sm:px-6 border hover:border-green hover:text-green  s:py-2 s:px-2 text-green-dark rounded focus:outline-none']"
   >
     <div v-if="isLoading">
       <img aria-label="loading-spinner" class="spinner" src="../../assets/loader.svg" />
