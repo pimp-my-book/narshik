832fa793bb123d7b30060d8b181bba2883837dd2
AmoDinho <u14284783@tuks.co.za>
2019-09-19 11:41:42 +0200

    added the test case for the button

---
+++ tests/unit/SecondaryButton.spec.js
@@ -0,0 +1,14 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import SecondaryButton from '../../src/components/buttons/SecondaryButton'
+
+afterEach(cleanup)
+
+describe('<SecondaryButton/>', () => {
+    test('It accepts button text prop', () => {
+
+    })
+
+    test('Shows loading state', () => {
+
+    })
+})
