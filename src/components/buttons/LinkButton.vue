84c5512e490301b40e33b6352f2118f3393953d0
AmoDinho <u14284783@tuks.co.za>
2019-09-19 12:26:45 +0200

    added inline stu\yles and props

--- src/components/buttons/LinkButton.vue
+++ src/components/buttons/LinkButton.vue
@@ -1,6 +1,13 @@
-<template></template>
+<template>
+  <p class="text-white font-body mr-4 hover:text-green">
+    <a href="http://app.gogoa.co.za/login" class="no-underline text-white hover:text-green">{{text}}</a>
+  </p>
+</template>
 <script>
 export default {
-  name: "LinkButton"
+  name: "LinkButton",
+  props: {
+    text: String
+  }
 };
 </script>
