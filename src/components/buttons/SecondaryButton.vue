b53e46f00c02841780d87ec6774cd8a8f7674d04
AmoDinho <u14284783@tuks.co.za>
2019-09-19 11:44:31 +0200

    added the loading spinner class

--- src/components/buttons/SecondaryButton.vue
+++ src/components/buttons/SecondaryButton.vue
@@ -5,7 +5,12 @@
     :class="[regular 
     ? ' hover:text-green-darker hover:bg-transparent bg-green-lighter font-body py-2 px-4 text-green-darker rounded border border-green hover:border-green hover:text-green focus:outline-none' 
     : ' hover:text-green-darker  hover:bg-transparent bg-green-lighter font-body lg:py-4 lg:px-6 sm:py-4 sm:px-6 border hover:border-green hover:text-green  s:py-2 s:px-2 text-green-darker rounded focus:outline-none']"
-  >{{text}}</button>
+  >
+    <div v-if="isLoading">
+      <img aria-label="loading-spinner" class="spinner" src="../../assets/loader.svg" />
+    </div>
+    <div v-else>{{text}}</div>
+  </button>
 </template>
 <script>
 export default {
