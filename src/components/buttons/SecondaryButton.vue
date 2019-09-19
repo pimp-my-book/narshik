a4d49a7b06863f42f9879f173bedcdbdb2b051f7
AmoDinho <u14284783@tuks.co.za>
2019-09-19 11:42:45 +0200

    created the SecondaryButton

---
+++ src/components/buttons/SecondaryButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    type="submit"
+    @click="forward"
+    :class="[regular 
+    ? ' hover:text-green-darker hover:bg-transparent bg-green-lighter font-body py-2 px-4 text-green-darker rounded border border-green hover:border-green hover:text-green focus:outline-none' 
+    : ' hover:text-green-darker  hover:bg-transparent bg-green-lighter font-body lg:py-4 lg:px-6 sm:py-4 sm:px-6 border hover:border-green hover:text-green  s:py-2 s:px-2 text-green-darker rounded focus:outline-none']"
+  >{{text}}</button>
+</template>
+<script>
+export default {
+  name: "SecondaryButton",
+  props: {
+    text: String,
+    url: String,
+    regular: Boolean
+  },
+  methods: {
+    forward() {
+      this.$router.push(this.url);
+    }
+  }
+};
+</script>
