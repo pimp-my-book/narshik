daad090052bf1dcc7fedffda4a859ef2f18dacd1
AmoDinho <u14284783@tuks.co.za>
2019-09-20 13:50:03 +0200

    added a test spec for the small card

---
+++ tests/unit/SmallCard.spec.js
@@ -0,0 +1,10 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import SmallCard from '../../src/components/cards/SmallCard'
+
+afterEach(cleanup)
+
+describe('<SmallCard>', () => {
+    test('It accepts the textprops', () => {
+
+    })
+})
