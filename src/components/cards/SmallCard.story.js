4239e33f55c6ac3df6aeb38d25ff0ccb7ca56667
AmoDinho <u14284783@tuks.co.za>
2019-09-20 13:58:08 +0200

    fixed issue with card stry

--- src/components/cards/SmallCard.story.js
+++ src/components/cards/SmallCard.story.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
 import SmallCard from './SmallCard.vue'
 
-storiesOf('Link Button', module)
+storiesOf('Small Card', module)
     .add('Basic Example', () => {
         return {
             components: { SmallCard },
-            template: '<SmallCard cardTitle="text-black" cardText="Do not cross us"/>'
+            template: '<div class="w-64"><SmallCard cardTitle="text-black" cardText="Do not cross us"/></div>'
         }
     })
