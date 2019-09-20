17b93b6c6e5555c6b1f477fba244ef5dd3a810f7
AmoDinho <u14284783@tuks.co.za>
2019-09-20 14:50:17 +0200

    added font boddy

--- src/components/modals/Alert.vue
+++ src/components/modals/Alert.vue
@@ -3,11 +3,9 @@
     <div>
       <span role="img" aria-label="error emoji">🚨</span>
     </div>
-    {{message}}
-    <span
-      aria-label="close button"
-      class="right-0 top-0 absolute mr-2 cursor-pointer"
-    >&times;</span>
+
+    <p aria-label="alert message" class="font-body">{message}</p>
+    <span aria-label="close button" class="right-0 top-0 absolute mr-2 cursor-pointer">&times;</span>
   </div>
 </template>
 <script>
