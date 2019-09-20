f3c5944cdf23b8a725ae40b5294046b5de4a90ff
AmoDinho <u14284783@tuks.co.za>
2019-09-20 14:11:19 +0200

    added test spec

---
+++ tests/unit/Alert.spec.js
@@ -0,0 +1,14 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import Alert from '../../src/components/modals/Alert.vue'
+
+afterEach(cleanup)
+
+describe('<Alert/>', () => {
+    test('Renders the alert message', () => {
+
+    })
+
+    test('Can close the alert', () => {
+
+    })
+})
