e0dd25e8faed249035808f50286450717ab0ce58
AmoDinho <u14284783@tuks.co.za>
2019-09-23 14:06:28 +0200

    added first test case

--- tests/unit/Alert.spec.js
+++ tests/unit/Alert.spec.js
@@ -5,7 +5,9 @@ afterEach(cleanup)
 
 describe('<Alert/>', () => {
     test('Renders the alert message', () => {
-
+        const message = 'Can you help me?'
+        const { queryByText } = render(Alert)
+        expect(queryByText(message))
     })
 
     test('Can close the alert', () => {
