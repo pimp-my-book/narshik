f6a90616a284a07f1fccf506b026be85609b6b72
AmoDinho <u14284783@tuks.co.za>
2019-09-26 15:38:35 +0200

    added a computed property

--- src/components/inputs/Input.vue
+++ src/components/inputs/Input.vue
@@ -1,38 +1,40 @@
 <template>
-    <input 
-    class="
-    bg-grey-lighter 
-    rounded-px 
-    py-2
-    px-4 
-    border-gray-500 
-    focus:outline-none 
-    focus:bg-white"
+  <input
+    class="bg-grey-lighter rounded-px py-2 px-4 border-gray-500 focus:outline-none focus:bg-white"
     type="text"
     :placeholder="placeholder"
     aria-label="input field"
     @input="handleInput"
-    />
+  />
 </template>
 <script>
 export default {
-    name:'Input',
-    props: {
-    placeholder:String,
-   },
-   data(){
-       return {
-           value: ""
-       }
-      
-   },
-    methods:{
-           handleInput(e){
-               this.$emit('input', e.target.value)
-           }
-       }
-   
-}
+  name: "Input",
+  props: {
+    placeholder: String
+  },
+  data() {
+    return {
+      newValue: ""
+    };
+  },
+  computed: {
+    computedValue: {
+      get() {
+        return this.newValue;
+      },
+      set(value) {
+        this.newValue = value;
+        this.$emit("input", value);
+      }
+    }
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit("input", e.target.value);
+    }
+  }
+};
 </script>
 <style>
 @import "../../assets/tailwind.css";
