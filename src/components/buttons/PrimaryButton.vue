65168cde1a64978611917c192785579698842b60
AmoDinho <u14284783@tuks.co.za>
2019-09-26 16:21:06 +0200

    v0.1.21

--- src/components/buttons/PrimaryButton.vue
+++ src/components/buttons/PrimaryButton.vue
@@ -13,7 +13,6 @@ forward - redirects using the url prop
     aria-label="primary-button"
     type="submit"
     url
-    @click="$emit('click',$event)"
     :class="[regular ? ' bg-green hover:text-green-dark hover:bg-green-lighter font-body py-2 px-4  text-white rounded focus:bg-green-lighter focus:outline-none' 
     : 'bg-green   hover:text-green-dark hover:bg-green-lighter font-body py-4 px-6 sl:py-4 sl:px-6 s:py-2 s:px-2 text-white rounded focus:outline-none ']"
   >
