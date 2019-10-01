c3fe1a282ca73073037718c515030bf2158724d6
AmoDinho <u14284783@tuks.co.za>
2019-10-01 11:37:51 +0200

    changed colour names

--- src/components/badges/Badge.vue
+++ src/components/badges/Badge.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     aria-label="badge"
-    :class="[success ? 'font-body text-sm text-green-lighter bg-green w-32 rounded-full py-1 text-center': warning ? 'font-body text-sm text-white bg-orange w-32 rounded-full py-1 text-center': 'font-body text-red-lighter text-sm bg-red w-32 rounded-full py-1 text-center']"
+    :class="[green ? 'font-body text-sm text-green-lighter bg-green w-32 rounded-full py-1 text-center': orange ? 'font-body text-sm text-white bg-orange w-32 rounded-full py-1 text-center': 'font-body text-red-lighter text-sm bg-red w-32 rounded-full py-1 text-center']"
   >
     <slot></slot>
   </div>
@@ -10,8 +10,8 @@
 export default {
   name: "Badge",
   props: {
-    success: Boolean,
-    warning: Boolean
+    green: Boolean,
+    orange: Boolean
   }
 };
 </script>
