91a1219c05371073fe7f50fc92dfdb42574cde1a
AmoDinho <u14284783@tuks.co.za>
2019-10-01 11:17:26 +0200

    added badge story

---
+++ src/components/badges/Badge.story.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { storiesOf } from '@storybook/vue'
+import Badge from './Badge.vue'
+
+storiesOf('Badge', module)
+    .add('Green', () => {
+        return {
+            components: { Badge },
+            template: '<Badge/>'
+        }
+    })
