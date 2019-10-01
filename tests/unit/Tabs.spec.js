aa1e11c80cad106a9b1270c565c8fe49a98a255d
AmoDinho <u14284783@tuks.co.za>
2019-10-01 12:12:04 +0200

    addded test case

---
+++ tests/unit/Tabs.spec.js
@@ -0,0 +1,9 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import Tab from '../../src/components/navigation/Tabs'
+
+afterEach(cleanup)
+describe('<Tab/>', () => {
+    test('It renders the selected tab', () => {
+
+    })
+})
