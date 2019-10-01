9d64e691db3fb2980cdd27e27489485c61c7cd95
AmoDinho <u14284783@tuks.co.za>
2019-10-01 11:15:32 +0200

    done with test case

--- tests/unit/Badge.spec.js
+++ tests/unit/Badge.spec.js
@@ -4,5 +4,11 @@ import Badge from '../../src/components/badges/Badge.vue'
 afterEach(cleanup)
 
 describe('<Badge/>', () => {
+    test('It renders the text message', () => {
 
+    })
+
+    test('It renders the correct colour', {
+
+    })
 })
