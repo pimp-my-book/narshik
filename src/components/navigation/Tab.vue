86d8b5fd9464a6528fd83e74910bbf7121c8b011
AmoDinho <u14284783@tuks.co.za>
2019-10-02 12:15:24 +0200

    added buefy tab

--- src/components/navigation/Tab.vue
+++ src/components/navigation/Tab.vue
@@ -1,6 +1,14 @@
-<template></template>
+<template>
+  <b-tabs>
+    <b-tab-item label="hello">hello</b-tab-item>
+  </b-tabs>
+</template>
 <script>
+import { Tabs } from "buefy";
 export default {
-  name: "Tab"
+  name: "Tab",
+  components: {
+    Tabs
+  }
 };
 </script>
