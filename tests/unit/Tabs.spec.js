85d922e6fd0d29729a691b4f745781891aa7ff4f
AmoDinho <u14284783@tuks.co.za>
2019-10-03 12:31:30 +0200

    added first test case

--- tests/unit/Tabs.spec.js
+++ tests/unit/Tabs.spec.js
@@ -1,10 +1,24 @@
-import { render, fireEvent, cleanup } from '@testing-library/vue'
+import { render, fireEvent, cleanup, getByText } from '@testing-library/vue'
 import Tab from '../../src/components/navigation/Tabs'
 
 afterEach(cleanup)
 describe('<Tab/>', () => {
     test('It renders the content in the tab', () => {
 
+        const headingOne = "Art"
+        const headingTwo = "Music"
+
+        const { getTextBy } = render(Tab, {
+            props: {
+                tabHeadingOne: headingOne,
+                tabHeadingTwo: headingTwo
+            },
+            slots: {
+                tabOne: 'I am designed this way'
+            }
+        })
+
+        expect(getByText('I am designed this way'))
     })
 
 
