994d5e04276af2b756946039860aa99231133e87
AmoDinho <u14284783@tuks.co.za>
2019-10-04 10:31:58 +0200

    added test cases

---
+++ tests/unit/Dropdown.spec.js
@@ -0,0 +1,14 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import Dropdown from '../../src/components/inputs/Dropdown.vue'
+
+afterEach(cleanup)
+
+describe('<Dropdown/>', () => {
+    test('It renders options in the dropdown list', () => {
+
+    })
+
+    test('It allows you to select an item in the options', () => {
+
+    })
+})
