159b314de59cc6b2da413a0730b21945194f60e8
AmoDinho <u14284783@tuks.co.za>
2019-10-04 11:39:55 +0200

    added test case

---
+++ tests/unit/Modal.spec.js
@@ -0,0 +1,14 @@
+import { render, fireEvent, cleanup } from '@testing-library/vue'
+import Modal from '../../src/components/modals/Modal.vue'
+
+afterEach(cleanup)
+
+describe('<Modal/>', () => {
+    test('It renders stuff in the slot', () => {
+
+    })
+
+    test('It closes the modal', () => {
+
+    })
+})
