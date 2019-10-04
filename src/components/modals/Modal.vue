c06e5c3d33352d5e22ea16d3858de4bfb0b9e958
AmoDinho <u14284783@tuks.co.za>
2019-10-04 11:44:41 +0200

    added modal structure

--- src/components/modals/Modal.vue
+++ src/components/modals/Modal.vue
@@ -1,6 +1,17 @@
-<template></template>
+<template>
+  <div>
+    <div>
+      <slot name="header"></slot>
+      <slot name="body"></slot>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
 <script>
 export default {
   name: "Modal"
 };
 </script>
+<style scoped>
+@import "../../assets/tailwind.css";
+</style>
