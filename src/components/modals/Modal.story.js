36598bd645a7a192cc454634c97ee60d801ae029
AmoDinho <u14284783@tuks.co.za>
2019-10-04 11:42:54 +0200

    added modal story

---
+++ src/components/modals/Modal.story.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { storiesOf } from '@storybook/vue'
+import Modal from './Modal.vue'
+
+storiesOf('Modal', module)
+    .add('Simple example', () => {
+        return {
+            components: { Modal },
+            template: '<Modal />'
+        }
+    })
