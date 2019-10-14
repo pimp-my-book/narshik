1ce5c1f203a172a44dcc7fd6ebb427fdfbbc0dd0
AmoDinho <u14284783@tuks.co.za>
2019-10-14 14:01:00 +0200

    updated story

--- src/components/modals/Modal.story.js
+++ src/components/modals/Modal.story.js
@@ -10,6 +10,9 @@ storiesOf('Modal', module)
             <template v-slot:header>
             Hello
             </template>
+            <template v-slot:body>
+            This is the modal's body
+            </template>
             </Modal>
             `
         }
