2182ed3eee50ca6ce979bf13139107207046d5e4
AmoDinho <u14284783@tuks.co.za>
2019-10-14 12:49:23 +0200

    adding comments

--- src/components/modals/Modal.vue
+++ src/components/modals/Modal.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="fixed top-0 right-0 bottom-0 left-0 flex justify-center align-center">
     <div>
-      <slot name="header"></slot>
+
+      <!-- -->
+
+      <!-- Header -->
+ <slot name="header"></slot>
+      <!--End of header -->
+     
       <slot name="body"></slot>
       <slot name="footer"></slot>
     </div>
