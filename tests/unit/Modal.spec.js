850b0cdf2ad1356735e2111fd46457b6fb034993
AmoDinho <u14284783@tuks.co.za>
2019-10-15 14:09:02 +0200

    done with first test case

--- tests/unit/Modal.spec.js
+++ tests/unit/Modal.spec.js
@@ -6,6 +6,22 @@ afterEach(cleanup)
 describe('<Modal/>', () => {
     test('It renders stuff in the slot', () => {
 
+        const headerSlot = `<h1>helo header</h1>`
+        const bodySlot = `<p>Formula One is...</p>`
+        const footerSlot = `<p>you know what...</p>`
+
+        const { queryByText } = render(Modal, {
+            slots: {
+                header: headerSlot,
+                body: bodySlot,
+                footer: footerSlot
+            }
+        })
+
+        expect(queryByText(headerSlot))
+        expect(queryByText(bodySlot))
+        expect(queryByText(footerSlot))
+
     })
 
     test('It closes the modal', () => {
